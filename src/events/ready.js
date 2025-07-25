d5f1f1b32b2816cdc89d5283cc616f39c0ece91f
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-07-25 17:28:01 -0400

    dnd

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,8 +1,18 @@
+const { ActivityType } = require("discord.js");
+
 module.exports = {
   name: "ready",
   once: true,
   async execute(client) {
-    const updates = await client.channels.fetch("1344570100589662268");
-    updates.send("I am now online/have been updated!");
+    client.user.setPresence({
+      status: "dnd"
+      /*activities: [
+        {
+          type: ActivityType.Custom,
+          name: "customstatus",
+          state: "",
+        },
+      ],*/
+    });
   },
 };
