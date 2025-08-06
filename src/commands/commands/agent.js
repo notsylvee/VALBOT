df63bf099f0c1c0b5f152de36ce4a5f2cf14bbb6
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-08-06 03:10:20 -0400

    + /agent

---
+++ src/commands/commands/agent.js
@@ -0,0 +1,26 @@
+const { EmbedBuilder } = require("discord.js");
+const fs = require("fs/promises");
+
+module.exports = {
+    data: {
+        name: "agent",
+        description: "Get a random Valorant agent",
+        "integration_types": [0, 1],
+        "contexts": [0, 1, 2]
+    },
+    async execute(interaction) {
+
+      const agentsJsonData = await fs.readFile("data/agents.json", {encoding: "utf8"});
+      const agentsMap = JSON.parse(agentsJsonData);
+      const agents = agentsMap["agents"];
+      const agent = agents[Math.floor(Math.random() * agents.length)];
+
+      const embed = new EmbedBuilder()
+      .setColor(`#ff4654`)
+      .setTitle(`${agent.name}`)
+      .setImage(`https://cdn.sylvee.xyz/valorantagent${agent.num}.png`)
+      .addFields({ name: '\u200b', value: `${agent.role}` })
+
+      await interaction.reply({ embeds: [embed] });
+    },
+};
